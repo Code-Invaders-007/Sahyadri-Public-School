158d525f420b5cc1f41f6466bfd671f0a2c07d3a
maruthi-syn-ack <80270578+maruthi-syn-ack@users.noreply.github.com>
2023-04-04 19:01:23 +0530

    Restructured Project files

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '8fd70d17641df352b12fe6168c8344912faaa911',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '8fd70d17641df352b12fe6168c8344912faaa911',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'stripe/stripe-php' => array(
+            'pretty_version' => 'v10.12.0',
+            'version' => '10.12.0.0',
+            'reference' => 'ef2b761e4c69de59892c95d0453e2caa25162faf',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../stripe/stripe-php',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);

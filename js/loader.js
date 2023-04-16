06ac3e65d5715a0dd0cfba320ae7b7039f659982
maruthi-syn-ack <80270578+maruthi-syn-ack@users.noreply.github.com>
2023-04-16 14:02:23 +0530

    Fixed nav-bar BUG 🪲

--- js/loader.js
+++ js/loader.js
@@ -9,15 +9,30 @@ function loadTemplate(url, callback) {
     xhr.send();
 }
 
+
 window.addEventListener('DOMContentLoaded', function () {
     var header = document.getElementById('header');
     var footer = document.getElementById('footer');
 
     loadTemplate('Components/header.html', function (html) {
         header.innerHTML = html;
+        hamMenu();
+
     });
 
     loadTemplate('Components/footer.html', function (html) {
         footer.innerHTML = html;
     });
 });
+
+function hamMenu() {
+    let navbar = document.querySelector('.header .navbar');
+
+    document.querySelector('#menu-btn').onclick = () => {
+        navbar.classList.toggle('active');
+    }
+
+    window.onscroll = () => {
+        navbar.classList.remove('active');
+    }
+}
